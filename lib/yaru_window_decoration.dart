e29bd46e05ec08c161b342aa39f08208f331c2eb
Paul Kepinski <paul.kepinski@orange.fr>
2023-02-15 16:15:04 +0100

    Import YaruWindowDecoration

--- lib/yaru_window_decoration.dart
+++ lib/yaru_window_decoration.dart
@@ -1,7 +1,4 @@
 library yaru_window_decoration;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/constants.dart';
+export 'src/yaru_window_decoration.dart';
